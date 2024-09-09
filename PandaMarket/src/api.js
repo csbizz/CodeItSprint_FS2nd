c34860ccf98561e0995601ebe43c4f7d3dda3065
csbizz <cheshirebizz@gmail.com>
2024-09-09 21:37:03 +0900

    sub2

--- PandaMarket/src/api.js
+++
@@ -1,9 +0,0 @@
-import * as ownFetch from './fetchUtils.js';
-
-const SERVER = 'https://panda-market-api.vercel.app/products';
-
-async function getProducts(params = {}) {
-  return await ownFetch.fetchGet(SERVER, params);
-}
-
-export { getProducts };
